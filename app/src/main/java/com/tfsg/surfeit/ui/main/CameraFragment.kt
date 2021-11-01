0874cb3867f88cfd7d0b989aca3273d40b9c4142
WWU <nguye230@wwu.edu>
2021-11-01 12:36:27 -0700

    testing username

--- app/src/main/java/com/tfsg/surfeit/ui/main/CameraFragment.kt
+++ app/src/main/java/com/tfsg/surfeit/ui/main/CameraFragment.kt
@@ -17,6 +17,7 @@ class CameraFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
         return inflater.inflate(R.layout.camera_fragment, container, false)
+        
     }
 
 }
